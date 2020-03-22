c9d53e58b39cc8e9b8fc231b6dab8a137a0179d4
Ridvan Baluyos <ridvan@baluyos.net>
2020-03-22 18:46:42 +0900

    add github repo link on footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>&copy; {{ date('Y') }} <a href="https://ncov.gundamserver.com" title="nCov-19 Tracker Philippines">ncov tracker ph</a> · <a href="#" title="source code">source code</a></span>
+            <span>&copy; {{ date('Y') }} <a href="https://ncov.gundamserver.com" title="nCov-19 Tracker Philippines">ncov tracker ph</a> · <a href="https://github.com/ridvanbaluyos/ph-ncov-tracker" title="source code">source code</a></span>
         </div>
     </div>
 </footer>
