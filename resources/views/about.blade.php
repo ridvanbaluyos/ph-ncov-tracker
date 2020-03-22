cfaa624374a0ea616113ac2f44fe539b5a4d088f
Ridvan Baluyos <ridvan@baluyos.net>
2020-03-22 18:57:12 +0900

    add tiktok hmmmm

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,14 +20,18 @@
                                     <p>
                                         So what makes this one unique from other trackers? Nothing really. I use the same data
                                         source as everyone else. If you think this website can help you visualize the situation,
-                                        then it has served its purpose. This website is under the <a href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a>.
+                                        then it has served its purpose.
                                     </p>
                                     <p>
                                         For any inconsistencies, please feel free to <a href="mailto:ridvan@baluyos.net">contact me</a>.
+                                        This website is under the <a href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a>.
                                     </p>
                                     <p>
                                         Stay safe and healthy, my friends!
                                     </p>
+                                    <div class="text-center">
+                                        <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@ridvanbaluyos/video/6803921097201863938" data-video-id="6803921097201863938" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@ridvanbaluyos" href="https://www.tiktok.com/@ridvanbaluyos">@ridvanbaluyos</a> <p></p> <a target="_blank" title="♬ မူရင်းအသံ  - Sang Kucing" href="https://www.tiktok.com/music/မူရင်းအသံ-Sang-Kucing-6758025524389497602">♬ မူရင်းအသံ  - Sang Kucing</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
+                                    </div>
                                 </div>
                             </div>
                         </div>
