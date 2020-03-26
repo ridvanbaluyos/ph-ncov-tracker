016e47fa972925f8a3384f5e3c31cb647e920b5c
Ridvan Baluyos <ridvan@baluyos.net>
2020-03-27 00:25:25 +0900

    add “medical” purposes in the disclaimer

--- resources/views/data-sources.blade.php
+++ resources/views/data-sources.blade.php
@@ -21,23 +21,23 @@
                                         The data extracted from these sources are believed to be reliable, no warranty, expressed or implied,
                                         is made regarding accuracy, adequacy, completeness, legality, reliability or usefulness of
                                         any information. The information is provided on an "as is" basis<sup><a href="https://infotrek.er.usgs.gov/doc/wdnr_biology/Public_Stocking/disclaimer.htm" target="_blank">[1]</a></sup>,
-                                        and may not be used for commercial purposes<sup><a href="https://systems.jhu.edu/research/public-health/ncov/" target="_blank">[2]</a></sup>. </small>
+                                        and may not be used for commercial and medical purposes<sup><a href="https://systems.jhu.edu/research/public-health/ncov/" target="_blank">[2]</a></sup>.
                                     </p>
-                                    <p>
-                                        For patients database and PH statistics:
-                                        <ul>
-                                            <li>
-                                                <a href="https://coronavirus-ph-api.now.sh" target="_blank">https://coronavirus-ph-api.now.sh</a>
-                                            </li>
-                                            <li>
-                                                <a href="https://www.reddit.com/r/Coronavirus_PH/comments/fehzke/ph_covid19_case_database_is_now_live/" target="_blank">https://www.reddit.com/r/Coronavirus_PH/comments/fehzke/ph_covid19_case_database_is_now_live/</a>
-                                            </li>
-                                        </ul>
-                                        For global statistics:
-                                        <ul>
-                                            <li><a href="https://github.com/mathdroid/covid-19-api" target="_blank">https://github.com/mathdroid/covid-19-api</a></li>
-                                        </ul>
-                                    </p>
+                                    For patients database and PH statistics:
+                                    <ul>
+                                        <li>
+                                            <a href="https://coronavirus-ph-api.now.sh" target="_blank">https://coronavirus-ph-api.now.sh</a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.reddit.com/r/Coronavirus_PH/comments/fehzke/ph_covid19_case_database_is_now_live/" target="_blank">https://www.reddit.com/r/Coronavirus_PH/comments/fehzke/ph_covid19_case_database_is_now_live/</a>
+                                        </li>
+                                    </ul>
+                                    For global statistics:
+                                    <ul>
+                                        <li>
+                                            <a href="https://github.com/mathdroid/covid-19-api" target="_blank">https://github.com/mathdroid/covid-19-api</a>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
